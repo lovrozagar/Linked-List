2404e948666bfbf2faeefa8a50f21aaf126419d8
lovro <lovro.zagar5@gmail.com>
2023-02-20 23:52:07 +0000

    feat: add insertAt and removeAt functions for linked list

--- index.js
+++ index.js
@@ -1,150 +1,204 @@
 class Node {
   constructor(data, next = null) {
-    this.data = data;
-    this.next = next;
+    this.data = data
+    this.next = next
   }
 }
 
 class LinkedList {
   constructor() {
-    this.head = null;
-    this.size = 0;
+    this.head = null
+    this.size = 0
   }
 
   prepend(data) {
-    this.head = new Node(data, this.head);
-    this.size += 1;
+    this.head = new Node(data, this.head)
+    this.size += 1
   }
 
   append(data) {
-    let node = new Node(data);
-    let current = this.head;
+    let node = new Node(data)
+    let current = this.head
 
     if (this.head === null) {
-      this.head = node;
+      this.head = node
     } else {
-      current = this.head;
+      current = this.head
 
       while (current.next) {
-        current = current.next;
+        current = current.next
       }
 
-      current.next = node;
+      current.next = node
     }
-    this.size += 1;
+    this.size += 1
   }
 
   pop() {
-    let current = this.head;
+    let current = this.head
 
     // IF ONLY ELEMENT IN LIST REMOVE IT AND RETURN
     if (current.next === null) {
-      this.head = null;
-      return;
+      this.head = null
+      return
     }
 
     while (current.next.next) {
-      current = current.next;
+      current = current.next
     }
 
-    current.next = null;
+    current.next = null
+  }
+
+  insertAt(index, data) {
+    if (index < 0 || index > this.size) {
+      return false
+    }
+
+    const newNode = new Node(data)
+
+    if (index === 0) {
+      newNode.next = this.head
+      this.head = newNode
+    } else {
+      let current = this.head
+      let previous = null
+      let i = 0
+
+      while (i < index) {
+        previous = current
+        current = current.next
+        i += 1
+      }
+
+      previous.next = newNode
+      newNode.next = current
+    }
+
+    this.size += 1
+    return true
+  }
+
+  removeAt(index) {
+    if (index < 0 || index >= this.size) {
+      return null
+    }
+
+    let current = this.head
+    let previous = null
+    let i = 0
+
+    while (i < index) {
+      previous = current
+      current = current.next
+      i += 1
+    }
+
+    if (previous === null) {
+      // removing the head node
+      this.head = current.next
+    } else {
+      previous.next = current.next
+    }
+
+    this.size -= 1
+    return current.data
   }
 
   contains(value) {
-    let current = this.head;
-    let isMatch = false;
+    let current = this.head
+    let isMatch = false
 
     while (current) {
       if (current.data === value) {
-        isMatch = true;
+        isMatch = true
       }
-      current = current.next;
+      current = current.next
     }
 
-    return isMatch;
+    return isMatch
   }
 
   find(value) {
-    let current = this.head;
-    let index = -1;
+    let current = this.head
+    let index = -1
 
     while (current) {
-      index += 1;
+      index += 1
       if (current.data === value) {
-        return index;
+        return index
       }
-      current = current.next;
+      current = current.next
     }
 
-    return null;
+    return null
   }
 
   getSize() {
-    return this.size;
+    return this.size
   }
 
   getHead() {
-    return this.head;
+    return this.head
   }
 
   getTail() {
-    let current = this.head;
+    let current = this.head
 
     while (current.next) {
-      current = current.next;
+      current = current.next
     }
 
-    return current;
+    return current
   }
 
   getNodeAtIndex(index) {
-    let current = this.head;
-    let counter = 0;
+    let current = this.head
+    let counter = 0
 
     while (current) {
       if (counter === index) {
-        return current;
+        return current
       }
-      counter += 1;
-      current = current.next;
+      counter += 1
+      current = current.next
     }
 
-    return `Node of index ${index} does not exist`;
+    return `Node of index ${index} does not exist`
   }
 
   printListValues() {
-    let current = this.head;
+    let current = this.head
 
     while (current) {
-      console.log(current.data);
-      current = current.next;
+      console.log(current.data)
+      current = current.next
     }
   }
 
   getListAsString() {
-    let current = this.head;
-    let string = '';
+    let current = this.head
+    let string = ''
 
     while (current) {
       if (current === this.head) {
-        string = string.concat(`(${current.data})`);
+        string = string.concat(`(${current.data})`)
       } else {
-        string = string.concat(` -> (${current.data})`);
+        string = string.concat(` -> (${current.data})`)
       }
-      current = current.next;
+      current = current.next
     }
 
-    string === '' ? (string = 'null') : (string = string.concat(' -> null'));
-    return string;
+    string === '' ? (string = 'null') : (string = string.concat(' -> null'))
+    return string
   }
 }
 
-const ll = new LinkedList();
-ll.append(100);
-ll.append(200);
-ll.append(300);
-ll.append(500);
-ll.append(400);
-ll.append(700);
-ll.printListValues();
-console.log(ll.getNodeAtIndex(0));
+const linkedList = new LinkedList()
+linkedList.prepend(100)
+linkedList.append(200)
+linkedList.append(300)
+linkedList.insertAt(1, 150)
+linkedList.removeAt(3)
+linkedList.printListValues()
+linkedList
--- tempCodeRunnerFile.js
+++
@@ -1,5 +0,0 @@
-ll.append(100);
-ll.append(200);
-ll.append(300);
-ll.append(500);
-ll.append(400);
